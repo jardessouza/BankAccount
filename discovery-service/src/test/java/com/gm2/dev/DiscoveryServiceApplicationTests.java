a90da87e15b440088c06a1f843dbe6c4ac6caa91
Jardes Andre <jardessouza@gmail.com>
2023-01-15 20:00:28 -0300

    Added discovery service

---
+++ discovery-service/src/test/java/com/gm2/dev/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gm2.dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
